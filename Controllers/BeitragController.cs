d48030cd60961505914073684627c499fd4a4e83
Andreas Jagdt <andreas.jagdt@roche.com>
2021-06-11 09:02:05 +0200

    Replace WeatherForecastController with BeitragController

---
+++ Controllers/BeitragController.cs
@@ -0,0 +1,40 @@
+﻿using ch.gibz.m151.projekt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ch.gibz.m151.projekt.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class BeitragController : ControllerBase
+    {
+        private readonly ILogger<BeitragController> _logger;
+
+        private BuenzliTreffContext _context = new BuenzliTreffContext();
+
+        public BeitragController(ILogger<BeitragController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Beitrag> Get()
+        {
+            BuenzliTreffContext buenzliTreffContext = new BuenzliTreffContext();
+            var Beitrags = buenzliTreffContext.Beitrags
+                .Include(b => b.Autor)
+                .Include(b => b.Titel)
+                .Include(b => b.Inhalt)
+                .Include(b => b.BeitragLikes)
+                .ToList();
+            return Beitrags;
+        }
+    }
+}
