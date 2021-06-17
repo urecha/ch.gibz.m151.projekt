f3ae310efd16550c518d45219733a76e37e928f8
URAN <urecha@emea.roche.com>
2021-06-17 12:44:18 +0200

    update article component6

--- ClientApp/src/app/article/article.component.ts
+++ ClientApp/src/app/article/article.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from 'src/data/services/article.service';
 import { Article } from '../../data/models/article';
 
 @Component({
@@ -7,13 +9,32 @@ import { Article } from '../../data/models/article';
   styleUrls: ['./article.component.css']
 })
 export class ArticleComponent implements OnInit {
-
-  @Input()
   article: Article;
 
-  constructor() { }
+  loading: boolean = true;
+
+  constructor(
+    private readonly articleService: ArticleService,
+    private readonly route: ActivatedRoute,
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(async (params) => {
+      if (params.id) {
+        if (params.id == 'new') {
+          this.article = new Article();
+        } else {
+          try {
+            this.loading = true;
+            this.article = await this.articleService.get(params.id).toPromise();
+          } catch (error) {
+            console.log(error);
+          } finally {
+            this.loading = false;
+          }
+        }
+      }
+    })
   }
 
 }
