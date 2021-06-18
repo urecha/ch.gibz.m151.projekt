78e124f88228063a1d5fa98463a7cdb4fd86a331
URAN <urecha@emea.roche.com>
2021-06-18 13:46:43 +0200

    update comment comp

--- ClientApp/src/app/comment/comment.component.ts
+++ ClientApp/src/app/comment/comment.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
 import { Comment } from 'src/data/models/comment';
+import { CommentService } from 'src/data/services/comment.service';
 
 @Component({
   selector: 'app-comment',
@@ -10,9 +12,21 @@ export class CommentComponent implements OnInit {
   @Input()
   comment: Comment;
 
-  constructor() { }
+  liked: boolean;
+
+  constructor(
+    private authorizeService: AuthorizeService,
+    private commentService: CommentService,
+  ) { }
 
   ngOnInit() {
+    this.authorizeService.getUser().toPromise().then(user => {
+      this.liked = this.comment.autor.name === user.name;
+    })
+  }
+
+  likeComment(){
+    this.commentService.likeComment(this.comment.id).subscribe(() => this.liked = !this.liked);
   }
 
 }
