5227df22ad9d0afb9bc395ad56e7a2f7cbc61ba4
Andreas Jagdt <andreas.jagdt@roche.com>
2021-06-18 10:19:00 +0200

    Add User DTO

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using ch.gibz.m151.projekt.Data;
+using ch.gibz.m151.projekt.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ch.gibz.m151.projekt.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        private ApplicationDbContext _context;
+
+        public UserController(ILogger<UserController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        [HttpGet]
+        [Route("User")]
+        public ApplicationUser Get(string id)
+        {
+            var user = _context.Users
+                .Where(u => u.Id == id)
+                .FirstOrDefault();
+            return user;
+        }
+
+    }
+}
---
+++ Models/DTO/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ch.gibz.m151.projekt.Models.DTO
+{
+    public class User
+    {
+        public User(ApplicationUser user)
+        {
+            Id = user.Id;
+            Name = user.UserName;
+            ArticleLikes = user.BeitragLikes;
+            CommentLikes = user.KommentarLikes;
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<BeitragLike> ArticleLikes { get; set; }
+        public ICollection<KommentarLike> CommentLikes { get; set; }
+
+        public int BuenzliRanking { get; set; }
+        public int HalbschuehRanking { get; set; }
+
+    }
+}
