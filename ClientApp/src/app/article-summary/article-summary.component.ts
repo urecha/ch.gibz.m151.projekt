6418b567e3c1fc09518a5040a31a3f282660095b
URAN <urecha@emea.roche.com>
2021-06-18 13:46:33 +0200

    update article summary comp

--- ClientApp/src/app/article-summary/article-summary.component.ts
+++ ClientApp/src/app/article-summary/article-summary.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { AuthorizeService } from 'src/api-authorization/authorize.service';
+import { ArticleService } from 'src/data/services/article.service';
 import { ArticleSummary } from '../../data/models/article';
 
 @Component({
@@ -10,9 +12,24 @@ export class ArticleSummaryComponent implements OnInit {
   @Input()
   article: ArticleSummary;
 
-  constructor() { }
+  username: string;
+
+  liked: boolean;
+
+  constructor(
+    private authorizeService: AuthorizeService,
+    private articleService: ArticleService,
+  ) { }
 
   ngOnInit() {
+    this.authorizeService.getUser().toPromise().then(user => {
+      this.username = user.name;
+      this.liked = this.article.autor.name == this.username;
+    })
+  }
+
+  likeArticle(){
+    this.articleService.likeArticle(this.article.id).subscribe(() => this.liked = !this.liked);
   }
 
 }
