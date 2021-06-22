5710b1715c4cfc89cea7e9e9e527817a3a86d46b
Andreas Jagdt <andreas.jagdt@roche.com>
2021-06-22 15:35:26 +0200

    Add KommentarService and DTO

---
+++ Business/KommentarLogic/IKommentarService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ch.gibz.m151.projekt.Models;
+using ch.gibz.m151.projekt.Models.DTO;
+
+namespace ch.gibz.m151.projekt.Business.KommentarLogic
+{
+    public interface IKommentarService
+    {
+        public List<Comment> GetForArticle(string id);
+
+        public void DeleteComment(int id);
+
+        public CreatedKommentar CreateOrUpdate(Comment incomingComment);
+
+        public Beitrag GetBeitrag(int id);
+    }
+}
